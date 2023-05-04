5e3205c0fb51ed18161a0d54bb35c3f453d42de9
fcroscatto <fcroscatto@gmail.com>
2023-05-03 21:12:17 -0300

    variaveis-exercicios

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,14 +1,16 @@
 // declarar uma variavel para armazenar um nome, uma para idade e o tipo das variaveis
-//let nome1 = Xampson
-//let idade1 = 12 
-//console.log(typeof nome1, "e", typeof idade1) 
+const nome1 = "Xampson"
+const idade1 = "18"
+console.log(typeof nome1, "e", typeof idade1) 
+
+// foi impresso o tipo string para ambas por ser uma constante
 
 let nome = prompt("Digite seu nome: ")
 let idade = Number(prompt("Digite sua idade: "))
 console.log(typeof nome, "e" , typeof idade)
 console.log("Olá, " ,nome,"você tem" , idade ,"anos.")
 
-// foram impressos string para letras e number para numeros
+// foram impressos string para letras e number para numeros pela determinação da digitação do usuario
 
 
 
---
+++ exercicio3/script.js
@@ -0,0 +1,35 @@
+// programa de RH 
+let nome = prompt("Digite seu nome completo")
+const dataNascimento = prompt("Digite sua data de nascimento")
+let endereco = prompt("Seu endereço")
+const cpf = prompt("Seu CPF")
+let escolaridade = prompt("Escolaridade")
+let cnh = prompt("Possui CNH? ")
+let filhos = Number(prompt("Quantos filhos possue"))
+let cargo = prompt("Qual seu cargo atual")
+let salario = prompt("Qual seu salário atual")
+let comissao = confirm(" Recebe comissão?")
+let  anoDeAdmissao = Number(prompt( "Ano de Admissão")) 
+console.log(nome);
+console.log(dataNascimento);
+console.log(endereco);
+console.log(cpf);
+console.log(escolaridade);
+console.log(cnh);
+console.log(filhos);
+console.log(cargo);
+console.log(salario);
+console.log(comissao);
+console.log(anoDeAdmissao);
+console.log(typeof nome);
+console.log(typeof dataNascimento);
+console.log(typeof endereco);
+console.log(typeof cpf);
+console.log(typeof escolaridade);
+console.log(typeof cnh);
+console.log(typeof filhos);
+console.log(typeof cargo);
+console.log(typeof salario);
+console.log(typeof comissao);
+console.log(typeof anoDeAdmissao);
+
