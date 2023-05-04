c877a3dca53c9e984b510535421e2d77a32e197b
fcroscatto <fcroscatto@gmail.com>
2023-05-03 21:11:37 -0300

    variaveis-exercicios

---
+++ exercicio1/script.js
@@ -0,0 +1,14 @@
+// declarar uma variavel para armazenar um nome, uma para idade e o tipo das variaveis
+//let nome1 = Xampson
+//let idade1 = 12 
+//console.log(typeof nome1, "e", typeof idade1) 
+
+let nome = prompt("Digite seu nome: ")
+let idade = Number(prompt("Digite sua idade: "))
+console.log(typeof nome, "e" , typeof idade)
+console.log("Olá, " ,nome,"você tem" , idade ,"anos.")
+
+// foram impressos string para letras e number para numeros
+
+
+
